c0058b747c2576103173bbcbcd97d733988c5bd9
Jaybird9er <JameyKirk@gmail.com>
2017-07-26 16:03:09 +0900

    1st Quote Generator Draft
    Stuck on quote generator.

---
+++ js/quotes.js
@@ -0,0 +1,36 @@
+/**************
+Random Quotes
+***************/
+
+var quoteList = [
+	{
+		quote: 'Frankly, my dear, I don\'t give a damn.',
+		source: 'Clark Gable, as Rhett Butler',
+		citation: 'Gone with the Wind',
+		year: 1939
+	},
+		{
+		quote: 'I\'m gonna make him an offer he can\'t refuse.',
+		source: 'Marlon Brando, as Don Vito Corleone',
+		citation: 'The Godfather',
+		year: 1972
+	},
+		{
+		quote: 'You don\'t understand! I coulda had class. I coulda been a contender. I could\'ve been somebody, instead of a bum, which is what I am.',
+		source: 'Marlon Brando, as Terry Malloy',
+		citation: 'On the Waterfront',
+		year: 1954
+	},
+		{
+		quote: 'Toto, I\'ve a feeling we\'re not in Kansas anymore.',
+		source: 'Judy Garland, as Dorothy',
+		citation: 'The Wizard of Oz',
+		year: 1939
+	},
+		{
+		quote: 'Here\'s looking at you, kid.',
+		source: 'Humphrey Bogart, as Rick Blaine',
+		citation: 'Casablanca',
+		year: 1942
+	}
+]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+/*
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+*/
+
+
+/**************
+Variables
+***************/
+
+var randomQuote = '';
+
+/**************
+Functions
+***************/
+
+//Random Number Function
+
+function randomNum () {
+	return Math.floor( Math.random() * quoteList.length - 1) + 1;
+}
+
+//Random Quote Function
+
+function getRandomQuote() {
+	randomQuote = quoteList[randomNum()].quote;
+	randomSource = quoteList[randomNum()].source;
+
+}
+
+//Print Quote Function
+
+function printQuote( writeQuote ) {
+	var showQuote = getRandomQuote();
+}
