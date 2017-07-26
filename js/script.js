3bc7a0c075a84c792c9adb09636ffc5e5f721fe6
Jaybird9er <JameyKirk@gmail.com>
2017-07-26 20:35:38 +0900

    getRandomQuote Finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
-/*
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-*/
+
 
 
 /**************
@@ -11,6 +11,9 @@ Variables
 ***************/
 
 var randomQuote = '';
+var randomSource = '';
+var randomCitation = '';
+var randomYear = '';
 
 /**************
 Functions
@@ -24,14 +27,15 @@ function randomNum () {
 
 //Random Quote Function
 
-function getRandomQuote() {
-	randomQuote = quoteList[randomNum()].quote;
-	randomSource = quoteList[randomNum()].source;
-
+function getRandomQuote(newQuote) {
+	var newQuote = quoteList[randomNum()];
+	return newQuote;
 }
 
 //Print Quote Function
 
-function printQuote( writeQuote ) {
-	var showQuote = getRandomQuote();
+/*function printQuote( writeQuote ) {
+	var quote = document.getElementById('quote');
+	var quoteBoxDiv.innerHTML = writeQuote;
 }
+*/
