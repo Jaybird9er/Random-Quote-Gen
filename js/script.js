cde8c3208522895481da02cac110d2c994554231
Jaybird9er <JameyKirk@gmail.com>
2017-07-27 20:43:28 +0900

    Completed innerHTML Issue

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
 
 /**************
 Variables
 ***************/
-
-/*
-var randomQuote = '';
-var randomSource = '';
-var randomCitation = '';
-var randomYear = '';
-*/
 
 /**************
 Functions
@@ -38,12 +28,11 @@ function getRandomQuote(newQuote) {
 
 function printQuote( output ) {
 	var printOutput = getRandomQuote();
-	var outputString = '<p class="quote">' + printOutput.quote + '</p>';
-	outputString += '<p class="source">' + printOutput.source;
-  outputString += '<span class="citation">' + printOutput.citation + '</span>';
-  outputString += '<span class="year">' + printOutput.year + '</span></p>';
+		var outputString = '<p class="quote">' + printOutput.quote + '</p>';
+		outputString += '<p class="source">' + printOutput.source;
+	  outputString += '<span class="citation">' + printOutput.citation + '</span>';
+	  outputString += '<span class="year">' + printOutput.year + '</span></p>';
 	var output = outputString;
+	document.getElementById('quote-box').innerHTML = output;
 	return output;
 }
-
-printQuote(document.getElementById('quote-box').innerHTML);
